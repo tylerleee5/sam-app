2eab1cc6c06e81389ce9b6c99bbf116455cabfbc
EC2 Default User <ec2-user@ip-172-31-12-187.us-west-2.compute.internal>
2025-03-21 20:36:26 +0000

    Breaking the lambda function on purpose

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -63,7 +63,7 @@ def apigw_event():
 
 
 def test_lambda_handler(apigw_event, mocker):
-    assert True 
+    assert True
 
     ret = app.lambda_handler(apigw_event, "")
     data = json.loads(ret["body"])
