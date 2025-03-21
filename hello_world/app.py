f6782a3e22d510f997ab156d328c9d044af45c51
EC2 Default User <ec2-user@ip-172-31-12-187.us-west-2.compute.internal>
2025-03-21 19:48:12 +0000

    Changed return message

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello my friend",
+            "message": "I'm using canary deployments",
             # "location": ip.text.replace("\n", "")
         }),
     }
